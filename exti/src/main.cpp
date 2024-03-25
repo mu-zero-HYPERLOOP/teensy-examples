5f46bf0dec52184920426f47622a630407eddb84
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2024-03-25 15:45:48 +0100

    Add first code for EXTI's.

--- exti/src/main.cpp
+++ exti/src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
 
-void setup() {
+const byte interruptPin = 2;
+const String errorMsg = "Missing edge detected!";
+int count = 0;
 
+void setup() {
+    pinMode(interruptPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), detectRisingEdge, RISING);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), detectFallingEdge, FALLING);
+}
+
+void increaseCount() {
+    ++count;
+    Serial.println(count);
+}
+
+void detectRisingEdge(){
+    ++count;
+}
+
+void detectFallingEdge(){
+    ++count;
+    Serial.println(count);
+    if (count % 2 == 1) {
+        Serial.println(errorMsg);
+    }
 }
 
 void loop() {
