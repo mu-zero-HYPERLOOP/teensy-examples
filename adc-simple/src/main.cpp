348f1b50a75d4e1558275b146e5347624df38bda
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-03-27 08:38:24 +0100

    implement naive adc read

--- adc-simple/src/main.cpp
+++ adc-simple/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-
+  Serial.begin(38400);
 }
 
 void loop() {
-
+  int val = analogRead(A0);
+  float val_volt = val * 3.3 / 1024;
+  Serial.print("analog value in volt: ");
+  Serial.print(val_volt);
+  delay(250);
 }
