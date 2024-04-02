3c296f8dec3d2d5f895a06fbdd385ab3d99e9691
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-04-02 23:30:48 +0200

    software trigger of etc works

--- adc-timed/src/main.cpp
+++ adc-timed/src/main.cpp
@@ -1,5 +1,6 @@
 #include "imxrt.h"
 #include "pins_arduino.h"
+#include "wiring.h"
 #include <Arduino.h>
 
 #define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
@@ -75,21 +76,21 @@ void flexpwm_init_signed() {     //set PWM
   FLEXPWM2_OUTEN |= FLEXPWM_OUTEN_PWMB_EN(0x0F); // Activate all B channels
   FLEXPWM2_MCTRL |= FLEXPWM_MCTRL_LDOK(0x0F);
         // Give Load Okay for all submodules -> reload setting again
-  FLEXPWM2_SM1TCTRL = FLEXPWM_SMTCTRL_OUT_TRIG_EN((1 << 4) | (1 << 5)); 
+  FLEXPWM2_SM1TCTRL = FLEXPWM_SMTCTRL_OUT_TRIG_EN((1 << 2) | (1 << 3)); 
         // val 4 of Flexpwm2 sm1 as trigger for PWM_OUT_TRIG0
         // val 5 of Flexpwm2 sm1 as trigger for PWM_OUT_TRIG1
 }
 
 void adc_init() {
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   analogRead(0);
   analogRead(1);
-  //ADC1_CFG |= ADC_CFG_ADTRG; // enable hardware trigger for conversion
-  ADC1_CFG &= ~ADC_CFG_ADTRG; // enable software trigger for conversion
-  ADC1_HC0 |= (1 << 7); // enable conversion complete interrupt
+  ADC1_CFG |= ADC_CFG_ADTRG; // enable hardware trigger for conversion
+  //ADC1_CFG &= ~ADC_CFG_ADTRG; // enable software trigger for conversion
   ADC1_HC0 = 0b10000; // set input select to external selection from ADC_ETC
   ADC1_HC1 = 0b10000; // same for hardware trigger control 1
+  //ADC1_HC0 |= 7; // channel 7 is A0
 }
 
 void adc_etc_init() {
@@ -100,18 +101,18 @@ void adc_etc_init() {
   ADC_ETC_CTRL |= ADC_ETC_CTRL_TRIG_ENABLE(0b11111111);
         // enable all external xbar triggers
 
-  ADC_ETC_TRIG0_CTRL = ADC_ETC_TRIG_CTRL_TRIG_CHAIN(2);
+  ADC_ETC_TRIG0_CTRL = ADC_ETC_TRIG_CTRL_TRIG_CHAIN(0);
         // set length of trigger chain 
         // (might be very interesting for many reads back to back)
         
-  ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_IE1(0b10);
-        // interrupt done1 when segment1 finished
-  ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_B2B1;
-        // trigger next conversion immediately after segment1 finished
-  ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_HWTS1(0b10);
-        // hardware trigger select of ADC_TRIG1
-  ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_CSEL1(8);
-        // select adc channel (A1)
+  //ADC_ETC_TRIG0_CHAIN_1_0 = ADC_ETC_TRIG_CHAIN_IE1(0b10);
+  //      // interrupt done1 when segment1 finished
+  //ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_B2B1;
+  //      // trigger next conversion immediately after segment1 finished
+  //ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_HWTS1(0b10);
+  //      // hardware trigger select of ADC_TRIG1
+  //ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_CSEL1(8);
+  //      // select adc channel
   ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_IE0(0b01);
         // interrupt done0 when segment0 finished
   ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_B2B0;
@@ -119,7 +120,7 @@ void adc_etc_init() {
   ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_HWTS0(0b1);
         // hardware trigger select of ADC_TRIG0
   ADC_ETC_TRIG0_CHAIN_1_0 |= ADC_ETC_TRIG_CHAIN_CSEL0(7);
-        // select adc channel (A0)
+        // select adc channel
   
   attachInterruptVector(IRQ_ADC_ETC0, adcetc0_isr);
   NVIC_ENABLE_IRQ(IRQ_ADC_ETC0);
@@ -172,6 +173,7 @@ void setup() {
 
 
 void loop() {
+  cli();
   FLEXPWM2_MCTRL |= FLEXPWM_MCTRL_CLDOK(0x0f);
 
   FLEXPWM2_SM0VAL3++;
@@ -182,6 +184,7 @@ void loop() {
   FLEXPWM2_SM1VAL3 = -FLEXPWM2_SM1VAL2;
 
   FLEXPWM2_MCTRL |= FLEXPWM_MCTRL_LDOK(0x0f);
+  sei();
 
   //Serial.print("sm0 val2: ");
   //Serial.println(FLEXPWM2_SM0VAL2);
@@ -193,6 +196,14 @@ void loop() {
   //Serial.print("sm1 val3: ");
   //Serial.println(FLEXPWM2_SM1VAL3);
   delay(10);
+  //Serial.println("loop still alive");
+
+
+
+  ADC_ETC_TRIG0_CTRL |= ADC_ETC_TRIG_CTRL_TRIG_MODE;
+  ADC_ETC_TRIG0_CTRL |= ADC_ETC_TRIG_CTRL_SW_TRIG;
+
+  Serial.println("aa");
 
 
 }
