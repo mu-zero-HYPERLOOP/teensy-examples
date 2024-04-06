376c1d8c3c1f316f3bdfea1c486b33653f052544
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2024-04-06 15:18:29 +0200

    First example of mass-flow-rate.

--- mass-flow-rate/src/main.cpp
+++ mass-flow-rate/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
 const byte massFlowPin = 2;
+const int flow = 100; //100ml
+
+volatile unsigned int presentRPM = 0;
 
 void massFlowISR() {
+  static unsigned long previousMicros = micros();                 // remember variable, initialize first time
+  unsigned long presentMicros = micros();                         // read microseconds
+  unsigned long revolutionTime = presentMicros - previousMicros;  // works fine with wrap-around of micros()
+  if (revolutionTime < 1000UL) return;                            // avoid divide by 0, also debounce, speed can't be over 60,000
+  presentRPM = (60000000UL / revolutionTime);                     // calculate
+  previousMicros = presentMicros;                                 // store microseconds for next time
+}
 
+int getFlowRate() {
+    return flow / presentRPM;
 }
 
 void setup() {
